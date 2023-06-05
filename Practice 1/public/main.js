551eef1f747d79b469a15218a048bdba8959b983
Rahul Kumar Mohanta <74450641+rahulx9@users.noreply.github.com>
2023-06-05 15:49:08 +0530

    multi color triangle changes

--- Practice 1/public/main.js
+++ Practice 1/public/main.js
@@ -8,18 +8,30 @@ if (!gl) {
 // create vertex data.
 const vertexData = [0, 1, 0, 1, -1, 0, -1, -1, 0];
 
+const colorData = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+
 // create buffer.
-const buffer = gl.createBuffer();
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+const vertexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
 // create vertex shader
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(
   vertexShader,
   `
+  precision mediump float;
+
   attribute vec3 position;
+  attribute vec3 color;
+  varying vec3 vColor;
+
   void main() {
+    vColor = color;
     gl_Position = vec4(position, 1);
   }
 `
@@ -31,8 +43,11 @@ const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(
   fragmentShader,
   `
+  precision mediump float;
+  varying vec3 vColor;
+
   void main() {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(vColor , 1);
   }
 `
 );
@@ -46,7 +61,13 @@ gl.linkProgram(program);
 
 const positionLocation = gl.getAttribLocation(program, "position");
 gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+const colorLocation = gl.getAttribLocation(program, "color");
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
 gl.drawArrays(gl.TRIANGLES, 0, 3);
